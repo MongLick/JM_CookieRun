6af2ddedcc998287a09a2dc1ad3ec02c88152d74
MongLick <qkjm4836@gmail.com>
2024-03-13 18:00:58 +0900

    feat : csv, 프리팹 1차

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,48 +1,45 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.Rendering;
 
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] PooledObject DoubleJumpObstacle;
-    [SerializeField] PooledObject GoldCoin;
-    [SerializeField] PooledObject GoldCoinEffect;
-    [SerializeField] PooledObject Growing;
-    [SerializeField] PooledObject JumpObstacle;
-    [SerializeField] PooledObject Magnet;
-    [SerializeField] PooledObject MakeCoin;
-    [SerializeField] PooledObject MakeJelly;
-    [SerializeField] PooledObject PinkJelly;
-    [SerializeField] PooledObject PinkJellyEffect;
-    [SerializeField] PooledObject Potion;
-    [SerializeField] PooledObject ResumeUI;
-    [SerializeField] PooledObject RunningEffect;
-    [SerializeField] PooledObject RunningFast;
-    [SerializeField] PooledObject SilverCoin;
-    [SerializeField] PooledObject SilverCoinEffect;
-    [SerializeField] PooledObject SildeObstacle;
-    [SerializeField] PooledObject YellowJelly;
-    [SerializeField] PooledObject YellowJellyEffect;
+    [SerializeField] PooledObject FirstPattern;
 
     private void Start()
     {
-        Manager.Pool.CreatePool(DoubleJumpObstacle, 30, 50);
-        Manager.Pool.CreatePool(GoldCoin, 30, 50);
-        Manager.Pool.CreatePool(GoldCoinEffect, 30, 50);
-        Manager.Pool.CreatePool(Growing, 30, 50);
-        Manager.Pool.CreatePool(JumpObstacle, 30, 50);
-        Manager.Pool.CreatePool(Magnet, 30, 50);
-        Manager.Pool.CreatePool(MakeCoin, 30, 50);
-        Manager.Pool.CreatePool(MakeJelly, 30, 50);
-        Manager.Pool.CreatePool(PinkJelly, 30, 50);
-        Manager.Pool.CreatePool(PinkJellyEffect, 30, 50);
-        Manager.Pool.CreatePool(Potion, 30, 50);
-        Manager.Pool.CreatePool(ResumeUI, 30, 50);
-        Manager.Pool.CreatePool(RunningEffect, 30, 50);
-        Manager.Pool.CreatePool(RunningFast, 30, 50);
-        Manager.Pool.CreatePool(SilverCoin, 30, 50);
-        Manager.Pool.CreatePool(SilverCoinEffect, 30, 50);
-        Manager.Pool.CreatePool(SildeObstacle, 30, 50);
-        Manager.Pool.CreatePool(YellowJelly, 30, 50);
-        Manager.Pool.CreatePool(YellowJellyEffect, 30, 50);
+        Manager.Pool.CreatePool(FirstPattern, 2, 4);
+
+        float x = datas.stage[0].JellyXpos;
+        float y = datas.stage[0].JellyYpos;
+
+        Manager.Pool.GetPool(FirstPattern, new Vector3(x, y, 0), Quaternion.identity);
+    }
+
+    public TextAsset data;
+    private AllData datas;
+
+    private void Awake()
+    {
+        datas = JsonUtility.FromJson<AllData>(data.text);
+
+        foreach (var VARIABLE in datas.stage)
+        {
+
+        }
+    }
+
+    [System.Serializable]
+    public class AllData
+    {
+        public MapData[] stage;
+    }
+
+    [System.Serializable]
+    public class MapData
+    {
+        public int stageID;
+        public float JellyXpos;
+        public float JellyYpos;
     }
 }
